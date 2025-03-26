ac3bb8f770a34acbb17b2cfec3537cb09be24074
soheeshins <w27k3966@gmail.com>
2025-03-26 10:42:55 +0900

    git 파일 추가
    git 파일 추가

---
+++ ffff.py
@@ -0,0 +1,4 @@
+list = "Hello, Python"
+
+# print(list[7:9])
+print(list[-2:])
---
+++ index.py
@@ -0,0 +1,34 @@
+# 주석
+"""주석 입니다.
+주석 입니다.
+주성 입니다."""
+
+# 변수
+name = " 홍길동"  # 문자열
+age = 20  # 정수
+height = 170.5  # 실수
+
+print(name)
+print(age)
+print(height)
+
+# 여러 변수에 동식에 값을 할당
+x, y, z = 10, 20, 30
+
+print(x)
+print(y)
+print(z)
+
+# 여러 변수에 같은 값 할당
+a = b = c = 100
+
+print('c', c)
+print('a', a)
+
+# 올바른 변수 이름
+name = 10
+student_id = 1
+_age = 20
+Age = 30
+
+
